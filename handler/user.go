86b9f57145d4d6332291bb59e87c724044c6888b
Ilham <ilhamnurhakim378@gmail.com>
2020-11-19 13:57:10 +0700

    update

---
+++ handler/user.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	//tangkap input dari user
+	//mapp input dari user ke struct registerInput
+	//struct diatas kita passing sebagai parameter service
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		//kalo error
+
+		// memanggil func err di formatter.go
+		errors := helper.FormatValidationError(err)
+
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+	if err != nil {
+		//kalo error
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, "ini toked")
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
---
+++ user/service.go
@@ -0,0 +1,40 @@
+package user
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Service interface {
+	RegisterUser(input RegisterUserInput) (User, error)
+}
+
+//internal struct dan memiliki ketergantungan
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) RegisterUser(input RegisterUserInput) (User, error) {
+	user := User{}
+	user.Name = input.Name
+	user.Email = input.Email
+	user.Occupation = input.Occupation
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.MinCost)
+	if err != nil {
+		return user, err
+	}
+	user.PasswordHash = string(passwordHash)
+	fmt.Println(user)
+	user.Role = "user"
+	newUser, err := s.repository.Save(user)
+	if err != nil {
+		return newUser, err
+	}
+	return newUser, nil
+}
